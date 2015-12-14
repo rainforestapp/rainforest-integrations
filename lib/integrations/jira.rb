8c7fa34576a747f66de9359865a7d13b7d1bd4ac
Edward Paulet <espaulet@gmail.com>
2015-12-14 12:11:39 -0800

    reuse existing issues in jira if they are not marked as done

--- lib/integrations/jira.rb
+++ lib/integrations/jira.rb
@@ -9,26 +9,62 @@ module Integrations
     def send_event
       return unless SUPPORTED_EVENTS.include?(event_type)
 
+      label = case event_type
+      when 'webhook_timeout' then "RfRun#{run[:id]}"
+      when 'run_test_failure' then "RfTest#{payload[:failed_test][:id]}"
+      end
+
+      params = {
+        body: {
+          jql: "labels = #{label} AND status != 'Done' AND project = #{settings[:project_key]}"
+        }.to_json
+      }
+
+      response = HTTParty.post("#{jira_base_url}/rest/api/2/search/", params.merge(headers))
+      validate_response(response)
+
+      issues = response['issues']
+
+      if issues.length > 0
+        issue = issues.first
+        update_issue(issue['id'])
+      else
+        create_issue
+      end
+    end
+
+    private
+
+    def update_issue(issue_id)
+      params = {
+        body: {
+          update: { labels: [{ add: 'RepeatedFailures' }] },
+          fields: { priority: { name: 'High' } }
+        }.to_json
+      }
+
+      response = HTTParty.put("#{jira_base_url}/rest/api/2/issue/#{issue_id}", params.merge(headers))
+      validate_response(response)
+    end
+
+    def create_issue
       post_data = case event_type
       when 'webhook_timeout' then create_webhook_timeout_issue
       when 'run_test_failure' then create_test_failure_issue
       end
 
-      response = HTTParty.post("#{jira_base_url}/rest/api/2/issue/",
-        body: post_data.to_json,
-        headers: {
-          'Content-Type' => 'application/json',
-          'Accept' => 'application/json'
-        },
-        basic_auth: {
-          username: settings[:username],
-          password: settings[:password]
-        }
+      params = { body: post_data.to_json }
+      response = HTTParty.post(
+        "#{jira_base_url}/rest/api/2/issue/",
+        params.merge(headers)
       )
+      validate_response(response)
+    end
 
+    def validate_response(response)
+      return true if response.nil?
       case response.code
-      when 201
-        # yay, that worked!
+      when 201, 200
         true
       when 401
         raise Integrations::Error.new('user_configuration_error', 'Authentication failed. Wrong username and/or password. Keep in mind that your JIRA username is NOT your email address.')
@@ -38,8 +74,6 @@ module Integrations
         raise Integrations::Error.new('misconfigured_integration', 'Invalid request to the JIRA API.')
       end
     end
-
-    private
 
     def create_test_failure_issue
       test = payload[:failed_test]
@@ -51,6 +85,7 @@ module Integrations
           issuetype: {
             name: "Bug"
           },
+          labels: ["RfTest#{test[:id]}"],
           environment: run[:environment][:name]
         }
       }
@@ -65,6 +100,7 @@ module Integrations
           issuetype: {
             name: "Bug"
           },
+          labels: ["RfRun#{run[:id]}"],
           environment: run[:environment][:name]
         }
       }
@@ -74,6 +110,13 @@ module Integrations
       # MAKE SURE IT DOESN'T HAVE A TRAILING SLASH
       base_url = settings[:jira_base_url]
       base_url.last == "/" ? base_url.chop : base_url
+    end
+
+    def headers
+      {
+        basic_auth: { username: settings[:username], password: settings[:password] },
+        headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+      }
     end
   end
 end
