3e7d7a3764c6f59eac499f17b17ffe149cdcf57b
Edward Paulet <espaulet@gmail.com>
2016-01-08 16:13:25 -0800

    rip oauth out and move to main app

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   match '/*all', to: 'application#cors_preflight_check', via: [:options]
   resources :events, only: %i(index create)
   resources :integrations, only: %i(index show)
-
-  # OAuth Routes
-  post '/oauth/request-token', to: 'oauth#request_token'
-  get '/oauth/access-token', to: 'oauth#access_token'
 end
